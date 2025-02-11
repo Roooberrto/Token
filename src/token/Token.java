de96c696a890cd82f105eb463b19ae47ec4fc2ee
GaabviL <eduard.stoicea@educa.madrid.org>
2025-02-11 13:01:03 +0100

    cambio

--- src/token/Token.java
+++ src/token/Token.java
@@ -15,6 +15,8 @@ public class Token {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Algo para que cambie el codigo");
+        // Perro Sanchez
     }
     
 }
